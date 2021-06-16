c96a44444ee704f7aedcf2e2ff58393d9350a5fe
Kevin Minutti <minutti70@gmail.com>
2021-06-16 12:17:18 -0400

    main.py setup with streamlit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Elastic App")
+
+st.sidebar.write("Pick your stock, industry, commodity, or crypto")
+
+st.header("Just testing and flexing whats good")
+
+st.write("Hey how are you doing?")
