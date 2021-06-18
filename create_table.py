de8f574ef726ce340887f6b231c3680b8d5aa7a3
Kevin Minutti <minutti70@gmail.com>
2021-06-18 19:31:26 -0400

    created script for making stock and price tables in db

---
+++ create_table.py
@@ -0,0 +1,46 @@
+import psycopg2
+from config import config
+
+def create_tables():
+    """ Creating tables in PostgreSQL DB """
+    commands = (
+        """ 
+        CREATE TABLE  stock (
+            id SERIAL PRIMARY KEY, 
+            symbol TEXT NOT NULL UNIQUE,
+            name TEXT NOT NULL,
+            exchange TEXT NOT NULL
+        )
+        """, 
+        """ 
+        CREATE TABLE price_data (
+            id SERIAL PRIMARY KEY,
+            stock_id INTEGER NOT NULL,
+            date DATE NOT NULL,
+            open NUMERIC(7,2) NOT NULL,
+            high NUMERIC(7,2) NOT NULL,
+            low NUMERIC(7,2) NOT NULL,
+            close NUMERIC(7,2) NOT NULL,
+            volume INTEGER NOT NULL,
+            FOREIGN KEY (stock_id) REFERENCES stock (id)
+        )
+        """)
+
+    connection = None
+    try: 
+        params = config()
+        connection = psycopg2.connect(**params)
+        cursor = connection.cursor()
+        for command in commands:
+            cursor.execute(command)
+        cursor.close()
+        connection.commit()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+
+
+if __name__ == '__main__':
+     create_tables()
