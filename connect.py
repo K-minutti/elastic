0a360784d85e080c153447f5300284c1b6797d20
Kevin Minutti <minutti70@gmail.com>
2021-06-18 18:47:25 -0400

    connecting to postgresql db

---
+++ connect.py
@@ -0,0 +1,28 @@
+import psycopg2
+from config import config
+
+def connect():
+    connection = None
+    try:
+        params= config()
+        print('Connecting to the PostgresSQL database...')
+        connection = psycopg2.connect(**params)
+
+        cursor = connection.cursor()
+
+        print('PostgresSQL database version:')
+        cursor.execute('SELECT version()')
+
+        db_version = cursor.fetchone()
+        print(db_version)
+        cursor.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+            print('Database connection closed.')
+
+if __name__ == '__main__':
+    connect()
